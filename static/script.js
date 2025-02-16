3e7c7af40157c5218ac63be1bd206797c6eb85db
samudhan2008 <159536401+samudhan2008@users.noreply.github.com>
2025-02-16 12:07:27 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleBtn = document.getElementById("toggle-theme");
     const body = document.body;
 
     if (localStorage.getItem("theme") === "dark") {
         body.classList.add("dark-mode");
     }
-
-    toggleBtn.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
-        localStorage.setItem("theme", body.classList.contains("dark-mode") ? "dark" : "light");
-    });
 });
