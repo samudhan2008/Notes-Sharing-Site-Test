2f198c83fd510537a785f11c5cc1d1301aba6c48
samudhan2008 <159536401+samudhan2008@users.noreply.github.com>
2025-02-23 11:11:12 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const body = document.body;
-
-    if (localStorage.getItem("theme") === "dark") {
-        body.classList.add("dark-mode");
-    }
-});
